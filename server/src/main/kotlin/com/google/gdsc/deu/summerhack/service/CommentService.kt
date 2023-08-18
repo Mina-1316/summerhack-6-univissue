9b71b4e8eb56d62745ab702a8ebe61b754ea7f74
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2023-08-19 02:42:06 +0900

    CommentService 생성

---
+++ server/src/main/kotlin/com/google/gdsc/deu/summerhack/service/CommentService.kt
@@ -0,0 +1,28 @@
+package com.google.gdsc.deu.summerhack.service
+
+import com.google.gdsc.deu.summerhack.dto.comment.CreateCommentRequestDto
+import com.google.gdsc.deu.summerhack.entity.comment.Comment
+import com.google.gdsc.deu.summerhack.entity.post.Post
+import com.google.gdsc.deu.summerhack.entity.user.User
+import com.google.gdsc.deu.summerhack.repository.CommentRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(rollbackFor = [Exception::class])
+class CommentService(
+        private val commentRepository: CommentRepository,
+) {
+    fun createComment(commentData: CreateCommentRequestDto,
+                      userData: User, postData: Post): Comment {
+        val commentInfo = Comment(
+                content = commentData.comment,
+                userId = userData.id,
+                postId = postData.id,
+        )
+
+        return commentRepository.save(
+                commentInfo
+        )
+    }
+}
