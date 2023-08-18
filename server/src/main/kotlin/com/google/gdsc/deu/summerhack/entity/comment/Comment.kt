e3be0fda165f0e1604f3a7d1b7fa5b528fead028
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2023-08-18 23:14:04 +0900

    Entity 작성

---
+++ server/src/main/kotlin/com/google/gdsc/deu/summerhack/entity/comment/Comment.kt
@@ -0,0 +1,19 @@
+package com.google.gdsc.deu.summerhack.entity.comment
+
+import jakarta.persistence.*
+
+@Entity
+class Comment(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long = 0,
+
+        @Column(nullable = false, unique = false)
+        val content: String,
+
+        @Column(nullable = false, unique = false)
+        val userId: Long,
+
+        @Column(nullable = false, unique = false)
+        val postId: Long,
+) {
+}
