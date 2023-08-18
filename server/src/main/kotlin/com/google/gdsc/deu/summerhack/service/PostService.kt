97779fcb3dd4508f691a455a11517ea206e6b44f
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2023-08-19 02:54:31 +0900

    PostService 생성

---
+++ server/src/main/kotlin/com/google/gdsc/deu/summerhack/service/PostService.kt
@@ -0,0 +1,24 @@
+package com.google.gdsc.deu.summerhack.service
+
+import com.google.gdsc.deu.summerhack.dto.post.PostCreationRequestDto
+import com.google.gdsc.deu.summerhack.entity.post.Post
+import com.google.gdsc.deu.summerhack.entity.user.User
+import com.google.gdsc.deu.summerhack.repository.PostRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(rollbackFor = [Exception::class])
+class PostService(
+        private val postRepository: PostRepository,
+) {
+    fun createPost(postData: PostCreationRequestDto, userData: User): Post {
+        val postInfo = Post(
+                title = postData.title,
+                content = postData.content,
+                userId = userData.id,
+        )
+
+        return postRepository.save(postInfo)
+    }
+}
