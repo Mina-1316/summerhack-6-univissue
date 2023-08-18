44e7ea1b01e69aea395ed0c914002b115f78b458
Mina1316 <mina1316@imtsoft.org>
2023-08-18 17:00:15 +0900

    project: Spring boot application Initialization

---
+++ server/src/main/kotlin/com/google/gdsc/deu/summerhack/SummerhackGroup6Application.kt
@@ -0,0 +1,11 @@
+package com.google.gdsc.deu.summerhack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SummerhackGroup6Application
+
+fun main(args: Array<String>) {
+	runApplication<SummerhackGroup6Application>(*args)
+}
---
+++ server/src/test/kotlin/com/google/gdsc/deu/summerhack/SummerhackGroup6ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.google.gdsc.deu.summerhack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SummerhackGroup6ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
