93e8c29aac99403b4c883f52449916558c605cbd
Mina1316 <mina1316@imtsoft.org>
2023-08-18 20:13:07 +0900

    feat: 게시글 작성/조회를 위한 DTO 추가

---
+++ server/src/main/kotlin/com/google/gdsc/deu/summerhack/dto/post/PostCreationDto.kt
@@ -0,0 +1,17 @@
+package com.google.gdsc.deu.summerhack.dto.post
+
+/**
+ * 게시글 작성을 위한 데이터 클래스
+ */
+data class PostCreationDto(
+    /**
+     * 게시글 제목
+     */
+    val title: String,
+    /**
+     * 게시글 내용
+     */
+    val content: String,
+)
+
+// 게시글 작성 API에는 Response가 존재하지 않음
